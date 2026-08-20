66879c28ae8ff7a85b6b908f9eb63a7a7bdf6584
avelinonascimento-lgtm <avelino.nascimento@escolar.ifrn.edu.br>
2026-08-20 09:43:20 -0300

    mudei o javascript

--- scripts/introdução.js
+++ scripts/introdução.js
@@ -1,1 +1,15 @@
-alert("olá mundo!")
+alert("olá mundo!");
+
+var nasc = 1991;
+let nome = "pedrina";
+const viva = true;
+let altura = 1.64;
+
+
+if (viva) {
+    let saudacao = "Olá, " + nome + "!";
+    let mensagem = `altura: ${altura}m | Idade: ${2025-nasc}`;
+    alert(saudacao+'\n' +mensagem);
+} else {
+    alert("você não está viva!");
+}
