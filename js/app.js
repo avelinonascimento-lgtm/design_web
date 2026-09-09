61a7ebea79c81d19363dba6559981d54700b6b93
avelinonascimento-lgtm <avelino.nascimento@escolar.ifrn.edu.br>
2026-09-09 13:06:53 -0300

    opa

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
-var nas = 1991;
-let nome = "pedrina";
-const viva = true;
-
-function calcIdade(ano=2026){
-    let idade = ano - nas;
-    alert(`dentro de função - Idade ${idade}`);
-    return idade;
+let n1 = Number(prompt("Digite um numero:"));
+let n2 = Number(prompt("Digite um numero:"));
+let op;
+do{
+    op = prompt()
 }
-
-calcIdade();
-alert(`fora da funcao: chamado calcIdade ${calcIdade(2027)}`);
